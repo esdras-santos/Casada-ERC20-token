39ea9ee7d7c9bbe40f1f04d9c43d3c1ecbbf45bd
santoz <esdrassenasantos@gmail.com>
2021-02-18 13:07:14 -0300

    complet erc-20 token

--- contracts/CasadaToken.sol
+++ contracts/CasadaToken.sol
@@ -1,9 +1,10 @@
 pragma solidity ^0.5.0;
 
 contract CasadaToken {
-    string  public name = "Casada";
-    string  public symbol = "CAS";
-    string  public standard = "Casada v1.0";
+    string public name = "Casada";
+    string public symbol = "CAS";
+    uint8 public decimals = 4;
+
     uint256 public totalSupply;
 
     event Transfer(
@@ -12,22 +13,48 @@ contract CasadaToken {
       uint256 _value
     );
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
   
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
 
-        emit Transfer(msg.sender, _to, _value);
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value/10000);
+        balanceOf[msg.sender] -= _value/10000;
+        balanceOf[_to] += _value/10000;
+
+        emit Transfer(msg.sender, _to, _value/10000);
 
         return true;
     }
 
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value/10000;
+        emit Approval(msg.sender, _spender, _value/10000);
+        return true;        
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
+        require(_value/10000 <= balanceOf[_from]);
+        require(_value/10000 <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value/10000;
+        balanceOf[_to] += _value/10000;
+
+        allowance[_from][msg.sender] -= _value/10000;
+
+        emit Transfer(_from, _to, _value/10000);
+
+        return true;
+    }
 }
--- test/CasadaToken.js
+++ test/CasadaToken.js
@@ -53,6 +53,64 @@ contract('CasadaToken', function(accounts) {
         assert.strictEqual(balance.toNumber(),750000,"decucts amount from the sending account");
     });
 
+  });
+
+  it("approves tokens to delegated transfer", function(){
+    return CasadaToken.deployed().then(function(instance){
+      tokenInstance = instance;
+      return tokenInstance.approve.call(accounts[1],100);
+    }).then(function(success){
+      assert.strictEqual(success,true, "it returns true");
+      return tokenInstance.approve(accounts[1], 100, {from: accounts[0]});
+    }).then(function(receipt){
+      assert.strictEqual(receipt.logs.length, 1, "triggers one event");
+      assert.strictEqual(receipt.logs[0].event, "Approval", 'should be the "Approval" event');
+      assert.strictEqual(receipt.logs[0].args._owner, accounts[0], "logs the account the tokens are trasfered from");
+      assert.strictEqual(receipt.logs[0].args._spender, accounts[1], "logs the account the tokens are trasfered to");
+      assert.strictEqual(receipt.logs[0].args._value.toNumber(), 100, "logs the transfer amount");
+      return tokenInstance.allowance(accounts[0],accounts[1]);
+    }).then(function(allowance){
+      assert.strictEqual(allowance.toNumber(), 100, "stores the allowance for delegated transfer");
+
+    });
   })
+
+  it("handles delegated token transfer", function(){
+    return CasadaToken.deployed().then(function(instance){
+      tokenInstance = instance;
+      fromAccount = accounts[2];
+      toAccount = accounts[3];
+      spendingAccount = accounts[4];
+
+      return tokenInstance.transfer(fromAccount, 100, {from: accounts[0]});
+    }).then(function(receipt){
+      return tokenInstance.approve(spendingAccount,10,{from: fromAccount});
+    }).then(function(receipt){
+      return tokenInstance.transferFrom(fromAccount, toAccount, 9999, {from: spendingAccount});
+    }).then(assert.fail).catch(function(error){
+      assert(error.message.indexOf("revert") >= 0, "cannot transfer value larger than balance");
+      return tokenInstance.transferFrom(fromAccount,toAccount, 20 , {from: spendingAccount});
+    }).then(assert.fail).catch(function(error){
+      assert(error.message.indexOf("revert") >= 0, "cannot transfer value larger than approved amount");
+      return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, {from: spendingAccount});
+    }).then(function(success){
+      assert.strictEqual(success,true);
+      return tokenInstance.transferFrom(fromAccount, toAccount, 10, {from: spendingAccount});
+    }).then(function(receipt){
+      assert.strictEqual(receipt.logs.length, 1, "triggers one event");
+      assert.strictEqual(receipt.logs[0].event, "Transfer", 'should be the "Transer" event');
+      assert.strictEqual(receipt.logs[0].args._from, fromAccount, "logs the account the tokens are trasfered from");
+      assert.strictEqual(receipt.logs[0].args._to, toAccount, "logs the account the tokens are trasfered to");
+      assert.strictEqual(receipt.logs[0].args._value.toNumber(), 10, "logs the transfer amount");
+
+      return tokenInstance.balanceOf(fromAccount);
+    }).then(function(balance){
+      assert.strictEqual(balance.toNumber(),90, "deducts the amount from the sending account");
+
+      return tokenInstance.allowance(fromAccount,spendingAccount);
+    }).then(function(allowance){
+      assert.strictEqual(allowance.toNumber(), 0, "decucts the amount from the allowance");
+    });
+  });
   
 })
